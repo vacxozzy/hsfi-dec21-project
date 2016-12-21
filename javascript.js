045468cc108437b45a6db3ea47584fd292a57ad7
Marc Gugliuzza <marc.gugliuzza@gmail.com>
2016-12-21 16:46:49 +0000

    final v2

--- javascript.js
+++ javascript.js
@@ -1,13 +1,10 @@
-/*
-This function is fired when the webpage is loaded
-*/
 $(document).ready(function(){
    $('.all-content').hide(); 
+   $('#goals-content').show();
 });
 
 /*
-When someone clicks goals-button we should hide all-content 
-and then show only goals-content
+When someone clicks goals-button we should hide all-content and show only goals-content
 */
 $("#goals-button").click(function(){
     $('.all-content').hide();
@@ -16,10 +13,40 @@ $("#goals-button").click(function(){
 
 
 /*
-When someone clicks the info button we should hide all-content 
-and then show only info-content
+When someone clicks the info button we should hide all-content and show only info-content
 */
 $("#info-button").click(function(){
     $('.all-content').hide();
     $('#info-content').show();
+});
+
+$("#takeover-button").click(function(){
+    $('.all-content').hide();
+    $('#takeover-content').show();
+});
+
+$("#takeover-form-button").click(function(){
+    $("#site-title").html("Your site now belongs to: " + $("#takeover-input").val())
+    $('#site-title').css('background-color', '#990000');
+    $('#site-title').css('color', '#ffffff');
+});
+
+$("#past-button").click(function(){
+    $('.all-content').hide();
+    $('#past-content').show();
+});
+
+$("#action-button").click(function(){
+    $('.all-content').hide();
+    $('#action-content').show();
+});
+
+$("#dark-button").click(function(){
+    $('body').css('background-color', '#333333');
+    $('body').css('color', '#ffffff');
+    
+});
+
+$('#donotpress-button').click(function(){
+   alert('I TOLD YOU NOT TO PRES ME!') 
 });
