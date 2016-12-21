d4dc977ae0fe94d47f2864858038ccb8e7812f05
Kaila Santiago <ksantiago.student@hsfi.us>
2016-12-21 21:56:59 +0000

    hide and show started on dark theme button

--- javascript.js
+++ javascript.js
@@ -2,24 +2,30 @@
 This function is fired when the webpage is loaded
 */
 $(document).ready(function(){
-   $('.all-content').hide(); 
+ $("#goals-content").show();
+ $("#about-content").hide();
+ $("#past-projects").hide();
+ $("#takeover-content").hide();
 });
 
-/*
-When someone clicks goals-button we should hide all-content 
-and then show only goals-content
-*/
 $("#goals-button").click(function(){
-    $('.all-content').hide();
-    $('#goals-content').show();
-});
-
-
-/*
-When someone clicks the info button we should hide all-content 
-and then show only info-content
-*/
+     $("#goals-content").show();
+     $("#about-content").hide();
+     $("#past-projects").hide();
+     $("#takeover-content").hide();
+ });
+ 
 $("#info-button").click(function(){
-    $('.all-content').hide();
-    $('#info-content').show();
+   $("#about-content").show();
+   $("#goals-content").hide();
+   $("#past-projects").hide();
+   $("#takeover-content").hide();
+   
+$("#dark-button").click(function(){
+    ("body").css("color", "white");
+    ("body").css("background-color", "black");
 });
+    
+   
+});
+ 
