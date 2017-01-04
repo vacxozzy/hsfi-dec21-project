e92e8ec03d89bc7340376c998e25238719974466
Kaila Santiago <ksantiago.student@hsfi.us>
2017-01-04 22:08:46 +0000

    finished project

--- javascript.js
+++ javascript.js
@@ -2,11 +2,10 @@
 This function is fired when the webpage is loaded
 */
 $(document).ready(function(){
- $("#goals-content").show();
- $("#about-content").hide();
- $("#past-projects").hide();
- $("#takeover-content").hide();
-});
+    $("#goals-content").show();
+    $("#about-content").hide();
+    $("#past-projects").hide();
+    $("#takeover-content").hide();
 
 $("#goals-button").click(function(){
      $("#goals-content").show();
@@ -20,12 +19,35 @@ $("#info-button").click(function(){
    $("#goals-content").hide();
    $("#past-projects").hide();
    $("#takeover-content").hide();
-   
+ });
+ 
+$("#projects-button").click(function(){
+   $("#past-projects").show();
+   $("#goals-content").hide();
+   $("#about-content").hide();
+   $("#takeover-content").hide();
+ });
+
+$("#takeover-button").click(function(){
+    $("#takeover-content").show();
+    $("#past-projects").hide();
+    $("#goals-content").hide();
+    $("#about-content").hide();
+ });
+ 
 $("#dark-button").click(function(){
-    ("body").css("color", "white");
-    ("body").css("background-color", "black");
+    $("body").css("background-color", "#343638");
+    $("body").css("color", "white");
 });
     
-   
+$("#no-press").click(function(){
+   alert("You shouldn't have pressed that!");
+});
+
+$("#takeover-form-button").click(function(){
+   $("#site-title").html('<p>Your Site Now Belongs to:</p> '+ $("input").val()+'</p>');
+   $("#site-title").css("background-color", "#a00806");
+   $("#site-title").css("color", "white");
+});
 });
  
